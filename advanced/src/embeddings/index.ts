34e20961910c88dbd3048ea4ff9c0d4bfeb8590b
Danila Simonov <ElKornacio@users.noreply.github.com>
2025-01-10 15:43:33 +0100

    added Anthropic (Voyage) support

--- advanced/src/embeddings/index.ts
+++ advanced/src/embeddings/index.ts
@@ -3,6 +3,8 @@ import OpenAI from 'openai';
 export const getEmbedding = async (text: string, model: string, api_key: string) => {
 	if (model.startsWith('openai-')) {
 		return getEmbeddingOpenAI(text, model, api_key);
+	} else if (model.startsWith('voyage-')) {
+		return getEmbeddingVoyage(text, model, api_key);
 	}
 
 	return getNullVector();
@@ -26,4 +28,22 @@ export const getEmbeddingOpenAI = async (text: string, model: string, api_key: s
 	});
 
 	return response?.data[0]?.embedding || getNullVector();
+};
+
+export const getEmbeddingVoyage = async (text: string, model: string, api_key: string) => {
+	const response = await fetch('https://api.voyageai.com/v1/embeddings', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': `Bearer ${api_key}`,
+		},
+		body: JSON.stringify({
+			input: [text],
+			model,
+		}),
+	});
+
+	const data = await response.json();
+
+	return data?.data[0]?.embedding || getNullVector();
 };
